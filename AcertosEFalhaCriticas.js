5208dac4609b1e09d0ea1402f8eb83abf2fe8b7b
Rogério Silva <rogerioavsilva@gmail.com>
2020-08-15 20:44:24 -0300

    first version critials

---
+++ AcertosEFalhaCriticas.js
@@ -0,0 +1,62 @@
+const criticalHit =
+	{"critics": {
+	"hits":[
+		{
+		  "id" : "1", 
+		   "criticalType" : [{ 
+								"type": "contusao", 
+								"typeDescription": "Contusão",
+								"title": "Traqueia Esmagada", 
+								"description": "Dano Crítico, e o alvo fica incapacitado(lj p.288), não pode falar e respirar, até o início de seu próximo turno" 
+							},
+							{ 
+								"type": "perfuracao", 
+								"typeDescription": "Perfuração",
+								"title": "Mão Perfurada", 
+								"description": "Dano Crítico, e o alvo solta um item que tiver segurando. Esse membro não pode ser usado, até o início de seu próximo turno" 
+							},
+							{
+								"type": "cortante", 
+								"typeDescription": "Cortante",
+								"title": "Testa Cortada", 
+								"description": "Dano Crítico, e o alvo fica cego (ldj p.287), até o início de seu próximo turno." 
+							},
+							{
+								"type": "magico", 
+								"typeDescription": "Mágico",
+								"title": "Vunerabilidade Mágica", 
+								"description": "Dano Crítico, e o alvo tem vulnerabilidade a dano mágico (ldj p.197), até o próximo turno início de seu próximo turno." 
+							}]
+		},
+		{
+		  "id" : "2", 
+		   "criticalType" : [
+								{ 
+									"type": "contusao", 
+									"typeDescription": "Contusão",
+									"title": "Pancada na Barriga", 
+									"description": "Dano Crítico, e o alvo não consegue se alimentar ou ingerir líquidos, até receber tratamento." 
+								},
+								{ 
+									"type": "perfuracao", 
+									"typeDescription": "Perfuração",
+									"title": "Clático Pinçado", 
+									"description": "Dano Crítico, e o alvo faz um teste de resistência de constituição CD 15. Se falhar, fica impedido (ldj p.288), até receber tratamento." 
+								},
+								{
+									"type": "cortante", 
+									"typeDescription": "Cortante",
+									"title": "Coluna Machucada", 
+									"description": "Dano Crítico, e o alvo fica caído (ldj p.287), não podendo levantar, até receber tratamento." 
+								},
+								{
+									"type": "magico", 
+									"typeDescription": "Mágico",
+									"title": "Vunerabilidade Mágica", 
+									"description": "Dano Crítico, e o alvo tem seu tamanho reduzido, como na magia 'Aumentar/Reduzir' (ldj p.217), até receber tratamento." 
+								}
+							]
+		}
+	],
+	"failure":[]
+}};
