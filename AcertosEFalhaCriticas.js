3ffbfcd6cea3081d1f4881b79075ff55f25da668
Rogério Silva <rogerioavsilva@gmail.com>
2020-08-15 22:30:46 -0300

    more hits critics

--- AcertosEFalhaCriticas.js
+++ AcertosEFalhaCriticas.js
@@ -56,7 +56,124 @@ const criticalHit =
 									"description": "Dano Crítico, e o alvo tem seu tamanho reduzido, como na magia 'Aumentar/Reduzir' (ldj p.217), até receber tratamento." 
 								}
 							]
+		},
+		{
+		  "id" : "3", 
+		   "criticalType" : [
+								{ 
+									"type": "contusao", 
+									"typeDescription": "Contusão",
+									"title": "Ombro Deslocado", 
+									"description": "Dano Crítico, e o alvo não pode realizar um 'Encontrão' até receber tratamento." 
+								},
+								{ 
+									"type": "perfuracao", 
+									"typeDescription": "Perfuração",
+									"title": "Pulmão Perfurado", 
+									"description": "Dano Crítico, e o alvo adquire 2 níveis de exaustão (ldj p.288), que podem ser removidos com tratamento." 
+								},
+								{
+									"type": "cortante", 
+									"typeDescription": "Cortante",
+									"title": "Artéria Cortada", 
+									"description": "Dano Crítico, e o alvo faz um teste de resistência de constituição CD 15. Se falhar, sofre dano igual a metade dos pontos de vida restantes." 
+								},
+								{
+									"type": "magico", 
+									"typeDescription": "Mágico",
+									"title": "Afeto Mágico", 
+									"description": "Dano Crítico, e o alvo fica enfeitiçado (ldj´p.288) por você, até o início de seu próximo turno." 
+								}
+							]
 		}
 	],
-	"failure":[]
+	"failure":[
+		{
+		  "id" : "1", 
+		   "criticalType" : [
+								{ 
+									"type": "", 
+									"typeDescription": "",
+									"title": "", 
+									"description": "" 
+								},
+								{ 
+									"type": "", 
+									"typeDescription": "",
+									"title": "", 
+									"description": "" 
+								},
+								{ 
+									"type": "", 
+									"typeDescription": "",
+									"title": "", 
+									"description": "" 
+								},
+								{ 
+									"type": "", 
+									"typeDescription": "",
+									"title": "", 
+									"description": "" 
+								}
+							]
+		},
+		{
+		  "id" : "2", 
+		   "criticalType" : [
+								{ 
+									"type": "", 
+									"typeDescription": "",
+									"title": "", 
+									"description": "" 
+								},
+								{ 
+									"type": "", 
+									"typeDescription": "",
+									"title": "", 
+									"description": "" 
+								},
+								{ 
+									"type": "", 
+									"typeDescription": "",
+									"title": "", 
+									"description": "" 
+								},
+								{ 
+									"type": "", 
+									"typeDescription": "",
+									"title": "", 
+									"description": "" 
+								}
+							]
+		},
+		{
+		  "id" : "3", 
+		   "criticalType" : [
+								{ 
+									"type": "", 
+									"typeDescription": "",
+									"title": "", 
+									"description": "" 
+								},
+								{ 
+									"type": "", 
+									"typeDescription": "",
+									"title": "", 
+									"description": "" 
+								},
+								{ 
+									"type": "", 
+									"typeDescription": "",
+									"title": "", 
+									"description": "" 
+								},
+								{ 
+									"type": "", 
+									"typeDescription": "",
+									"title": "", 
+									"description": "" 
+								}
+							]
+		}
+	]
 }};
